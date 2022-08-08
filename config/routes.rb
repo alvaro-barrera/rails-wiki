278ded167df328ec298dbbf2c9204a0b3244940e
alvaro__barrera <alvaro_bv@hotmail.com>
2022-08-08 17:16:47 -0500

    Added wiki post scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :wiki_posts
   get 'welcome/index'
+  root "welcome#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
